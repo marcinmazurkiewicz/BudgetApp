24a95c2542ab8ca494db1d4df1c198ed84b965af
Marcin Mazurkiewicz <marcin.mazurkiewicz@ug.edu.pl>
2019-01-11 09:55:50 +0100

    Init commit

---
+++ app/src/main/java/sh/mazurkiewicz/budgetapp/MainActivity.java
@@ -0,0 +1,13 @@
+package sh.mazurkiewicz.budgetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
